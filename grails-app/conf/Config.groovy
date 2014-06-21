2b135a275bcf9230c2c1df5d1dfed8e64f979b77
Minhaj Mehmood <minhaj.kk@gmail.com>
2014-06-21 22:29:56 +0400

    Initial commit

---
+++ grails-app/conf/Config.groovy
@@ -0,0 +1,43 @@
+// configuration for plugin testing - will not be included in the plugin zip
+
+log4j = {
+    // Example of changing the log pattern for the default console
+    // appender:
+    //
+    //appenders {
+    //    console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
+    //}
+
+    error  'org.codehaus.groovy.grails.web.servlet',  //  controllers
+           'org.codehaus.groovy.grails.web.pages', //  GSP
+           'org.codehaus.groovy.grails.web.sitemesh', //  layouts
+           'org.codehaus.groovy.grails.web.mapping.filter', // URL mapping
+           'org.codehaus.groovy.grails.web.mapping', // URL mapping
+           'org.codehaus.groovy.grails.commons', // core / classloading
+           'org.codehaus.groovy.grails.plugins', // plugins
+           'org.codehaus.groovy.grails.orm.hibernate', // hibernate integration
+           'org.springframework',
+           'org.hibernate',
+           'net.sf.ehcache.hibernate'
+}
+
+
+// Added by the Spring Security Core plugin:
+grails.plugin.springsecurity.userLookup.userDomainClassName = 'com.scrum.auth.User'
+grails.plugin.springsecurity.userLookup.authorityJoinClassName = 'com.scrum.auth.UserRole'
+grails.plugin.springsecurity.authority.className = 'com.scrum.auth.Role'
+grails.plugin.springsecurity.controllerAnnotations.staticRules = [
+	'/':                              ['permitAll'],
+	'/index':                         ['permitAll'],
+	'/index.gsp':                     ['permitAll'],
+	'/**/js/**':                      ['permitAll'],
+	'/**/css/**':                     ['permitAll'],
+	'/**/images/**':                  ['permitAll'],
+	'/**/favicon.ico':                ['permitAll']
+]
+
+//grails.project.repos.myRepo.url = "http://staging.cobone.net/artifactory/"
+//grails.project.repos.myRepo.type = "maven"
+//grails.project.repos.myRepo.username = "admin"
+//grails.project.repos.myRepo.password = "password"
+//grails.project.repos.myRepo.portal = "grailsCentral"
